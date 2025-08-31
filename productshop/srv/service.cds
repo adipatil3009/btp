ff617d0bc7db3c4718552369ebc6f2b3bb539202
Aditya Patil <adirpatilisl@gmail.com>
2025-08-31 06:47:56 +0000

    12:15 | 31-08

--- productshop/srv/service.cds
+++ productshop/srv/service.cds
@@ -1,4 +1,12 @@
+@protocol: 'rest'
+
 service product {
     function printhelloworld(input : String) returns String;
+
+    function addition(num1 : Integer, num2 : Integer) returns Integer;
+
+    @open
+    type object {};
+    function myFunction(catogery : Integer) returns object;    
 }
 
--- productshop/srv/service.js
+++ productshop/srv/service.js
@@ -8,5 +8,28 @@ module.exports = cds.service.impl(async function (srv) {
 
         return `${req.data.input} world`
     } )
+
+    srv.on('addition', req=>{
+        console.log(req.data)
+        let result = req.data.num1 + req.data.num2;
+        return result;
+    })
+
+    srv.on('myFunction', req=>{
+        let result ={}
+
+        if (req.data.catogery == 1 ) {
+            result.product = "BMW",
+            result.price = '12000'
+            result.location = 'New Delhi'
+        }
+        else {
+            result.product = "Audi",
+            result.price = '10000'
+            result.location = 'Mumbai'
+        }
+
+        return result;
+    })
 })
 
